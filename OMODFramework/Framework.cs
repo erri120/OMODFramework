02cd45390570b524ddd581b742eefe9856db922c
erri120 <erri120@protonmail.com>
2019-11-24 18:17:49 +0100

    Added Fake OBMM versions and max stream size settings to the Framework

--- OMODFramework/Framework.cs
+++ OMODFramework/Framework.cs
@@ -4,6 +4,16 @@ namespace OMODFramework
 {
     public class Framework
     {
+        internal string Version = "1.1.12";
+        internal byte MajorVersion = 1;
+        internal byte MinorVersion = 1;
+        internal byte BuildNumber = 12;
+        internal byte CurrentOmodVersion = 4;
+
+        internal bool IgnoreVersion = false;
+
+        internal static int MaxMemoryStreamSize => 67108864;
+
         internal static string TempDir { get; set; } = Path.Combine(Path.GetTempPath(), "OMODFramework");
     }
 }
