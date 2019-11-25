ae4aed9cb49ef9d041e4fe721f599d29f063f276
erri120 <erri120@protonmail.com>
2019-11-25 15:35:26 +0100

    Updated Assembly Information

--- OMODFramework/Properties/AssemblyInfo.cs
+++ OMODFramework/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,9 +7,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("OMODFramework")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Framework for working with .omod files")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("erri120")]
 [assembly: AssemblyProduct("OMODFramework")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
@@ -35,3 +36,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: InternalsVisibleTo("OMODFramework.Test")]
+[assembly: NeutralResourcesLanguage("en")]
