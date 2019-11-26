0e567cadef0606c876bdaecd7ef560880c763c0d
erri120 <erri120@protonmail.com>
2019-11-26 12:15:51 +0100

    Created Example project

---
+++ OMODFramework.Example/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace OMODFramework.Example
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // set the temp folder if you don't want it at %temp%/OMODFramework/
+            Framework.TempDir = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+
+            var omod = new OMOD("DarNified UI 1.3.2.omod");
+
+            var data = omod.GetDataFiles(); // extracts all data files
+            var plugins = omod.GetPlugins(); // extracts all plugins
+
+            Console.WriteLine($"Data files: {data}");
+            Console.WriteLine($"Plugins: {plugins}");
+        }
+    }
+}
