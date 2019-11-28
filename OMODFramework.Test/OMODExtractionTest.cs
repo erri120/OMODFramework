7b9417e08f926bf85b2aaad171b52e31e5ea0672
erri120 <erri120@protonmail.com>
2019-11-28 20:05:04 +0100

    Updated Pipeline

--- OMODFramework.Test/OMODExtractionTest.cs
+++ OMODFramework.Test/OMODExtractionTest.cs
@@ -5,6 +5,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace OMODFramework.Test
 {
+    [TestCategory("OMODExtraction")]
     [TestClass]
     public class OMODExtractionTest : ATest
     {
