c607d3f4903b05d11b8770f36fc2b3595fb1ea4a
erri120 <erri120@protonmail.com>
2019-11-30 16:03:37 +0100

    Updated the example

--- OMODFramework.Example/Program.cs
+++ OMODFramework.Example/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Reflection;
 
 namespace OMODFramework.Example
@@ -10,13 +11,22 @@ namespace OMODFramework.Example
             // set the temp folder if you don't want it at %temp%/OMODFramework/
             Framework.TempDir = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "TestTempDir");
 
+            Framework.OblivionINIFile = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+                "Oblivion.ini");
+
+            if(Directory.Exists(Framework.TempDir))
+                Framework.CleanTempDir();
+
             var omod = new OMOD("DarNified UI 1.3.2.omod");
 
             var data = omod.GetDataFiles(); // extracts all data files
             var plugins = omod.GetPlugins(); // extracts all plugins
 
             var scriptFunctions = new ScriptFunctions();
-            omod.RunScript(scriptFunctions, data, plugins);
+            var srd = omod.RunScript(scriptFunctions, data, plugins);
+
+            if(srd.CancelInstall)
+                Console.WriteLine("Canceled");
         }
     }
 }
