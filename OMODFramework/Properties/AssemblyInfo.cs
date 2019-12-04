feb257b59523daf9979bebd261e2b63ee01de6e4
erri120 <erri120@protonmail.com>
2019-12-04 16:02:23 +0100

    Version bump to 1.1.2

--- OMODFramework/Properties/AssemblyInfo.cs
+++ OMODFramework/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("erri120")]
 [assembly: AssemblyProduct("OMODFramework")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.1.0")]
-[assembly: AssemblyFileVersion("1.1.1.0")]
+[assembly: AssemblyVersion("1.1.2.0")]
+[assembly: AssemblyFileVersion("1.1.2.0")]
 [assembly: InternalsVisibleTo("OMODFramework.Test")]
 [assembly: NeutralResourcesLanguage("en")]
