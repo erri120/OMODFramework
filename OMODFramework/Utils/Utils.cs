33d62fc1cd158b940da255906ddcd107681cbeff
erri120 <erri120@protonmail.com>
2020-06-04 13:57:43 +0200

    Cleanup

--- OMODFramework/Utils/Utils.cs
+++ OMODFramework/Utils/Utils.cs
@@ -6,6 +6,22 @@ namespace OMODFramework
 {
     internal static partial class Utils
     {
+        internal static string ToFileString(this OMODEntryFileType entryFileType)
+        {
+            return entryFileType switch
+            {
+                OMODEntryFileType.DataCRC => "data.crc",
+                OMODEntryFileType.PluginsCRC => "plugins.crc",
+                OMODEntryFileType.Config => "config",
+                OMODEntryFileType.Readme => "readme",
+                OMODEntryFileType.Script => "script",
+                OMODEntryFileType.Image => "image",
+                OMODEntryFileType.Data => "data",
+                OMODEntryFileType.Plugins => "plugins",
+                _ => throw new ArgumentOutOfRangeException(nameof(entryFileType), entryFileType, "Should not be possible!")
+            };
+        }
+
         internal static void Debug(string s)
         {
 
